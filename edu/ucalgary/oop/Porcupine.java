e83504550e380f72b1e2f6b3f40e3ff7c0475852
AarshShah9 <aarshshah99@gmail.com>
2023-03-18 21:33:51 -0600

    added animal sub classes

---
+++ edu/ucalgary/oop/Porcupine.java
@@ -0,0 +1,15 @@
+package edu.ucalgary.oop;
+
+import java.time.LocalTime;
+
+public class Porcupine extends Animal {
+    public static final LocalTime FEED_TIME = LocalTime.of(0, 5, 0, 0);
+    public static final LocalTime FEED_PREP_TIME = LocalTime.of(0, 0, 0, 0);
+    public static final LocalTime CLEAN_TIME = LocalTime.of(0, 10, 0, 0);
+
+    public Porcupine(int id, String animalName, String animalType, String animalFeedingType, Boolean orphaned)
+            throws IllegalArgumentException {
+        super(id, animalName, animalType, animalFeedingType, orphaned);
+    }
+
+}
