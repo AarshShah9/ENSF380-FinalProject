849b1cfd5ac835945a7f82a535693371805a6877
AarshShah9 <aarshshah99@gmail.com>
2023-03-18 21:53:14 -0600

    added task class

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -3,16 +3,23 @@ package edu.ucalgary.oop;
 import java.time.LocalTime;
 
 public abstract class Animal {
-    private int ANIMAL_ID;
-    private String ANIMAL_NAME;
-    private AnimalType ANIMAL_TYPE;
-    private FeedingType ANIMAL_FEEDING_TYPE;
-    private Boolean ORPHANED;
+    private final int ANIMAL_ID;
+    private final String ANIMAL_NAME;
+    private final AnimalType ANIMAL_TYPE;
+    private final FeedingType ANIMAL_FEEDING_TYPE;
+    private final Boolean ORPHANED;
     public static final LocalTime FEED_WINDOW = LocalTime.of(3, 0, 0);
 
     public Animal(int id, String animalName, String animalType, String animalFeedingType, Boolean orphaned)
             throws IllegalArgumentException {
         // Implement this constructor
+        // Change this up, it's just a placeholder to avoid final variable not
+        // initialized error
+        this.ANIMAL_ID = id;
+        this.ANIMAL_NAME = animalName;
+        this.ANIMAL_TYPE = AnimalType.valueOf(animalType);
+        this.ANIMAL_FEEDING_TYPE = FeedingType.valueOf(animalFeedingType);
+        this.ORPHANED = orphaned;
 
     }
 
---
+++ edu/ucalgary/oop/Task.java
@@ -0,0 +1,48 @@
+package edu.ucalgary.oop;
+
+public class Task {
+    private final int TASK_ID;
+    private final Boolean BACKUP_VOLUNTEER_NEEDED;
+    private final String DESCRIPTION;
+    private final int DURATION;
+    private final int MAX_WINDOW;
+    private final String TASK_TYPE;
+
+    public Task(int taskID, String description, Boolean volunteerNeeded, int duration, int maxWindow, String taskType) {
+        // TODO implement constructor
+        // Change this up, it's just a placeholder to avoid final variable not
+        // initialized error
+        this.TASK_ID = taskID;
+        this.DESCRIPTION = description;
+        this.BACKUP_VOLUNTEER_NEEDED = volunteerNeeded;
+        this.DURATION = duration;
+        this.MAX_WINDOW = maxWindow;
+        this.TASK_TYPE = taskType;
+
+    }
+
+    public Boolean getBackupVolunteerNeeded() {
+        return BACKUP_VOLUNTEER_NEEDED;
+    }
+
+    public String getDescription() {
+        return DESCRIPTION;
+    }
+
+    public int getDuration() {
+        return DURATION;
+    }
+
+    public int getMaxWindow() {
+        return MAX_WINDOW;
+    }
+
+    public String getTaskType() {
+        return TASK_TYPE;
+    }
+
+    public int getTaskID() {
+        return TASK_ID;
+    }
+
+}
