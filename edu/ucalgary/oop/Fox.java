12522fd849246cf2b34c402c916cd2943a97a131
AarshShah9 <aarshshah99@gmail.com>
2023-03-18 21:33:51 -0600

    added animal sub classes

--- edu/ucalgary/oop/Fox.java
+++ edu/ucalgary/oop/Fox.java
@@ -2,12 +2,12 @@ package edu.ucalgary.oop;
 
 import java.time.LocalTime;
 
-public class Coyote extends Animal {
+public class Fox extends Animal {
     public static final LocalTime FEED_TIME = LocalTime.of(0, 5, 0, 0);
-    public static final LocalTime FEED_PREP_TIME = LocalTime.of(0, 10, 0, 0);
+    public static final LocalTime FEED_PREP_TIME = LocalTime.of(0, 5, 0, 0);
     public static final LocalTime CLEAN_TIME = LocalTime.of(0, 5, 0, 0);
 
-    public Coyote(int id, String animalName, String animalType, String animalFeedingType, Boolean orphaned)
+    public Fox(int id, String animalName, String animalType, String animalFeedingType, Boolean orphaned)
             throws IllegalArgumentException {
         super(id, animalName, animalType, animalFeedingType, orphaned);
     }
