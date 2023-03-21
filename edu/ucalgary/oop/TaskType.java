543a8d78b3e5b4b259083bedecd88b8ad0f49671
AarshShah9 <aarshshah99@gmail.com>
2023-03-21 14:32:22 -0600

    Addeed documentation to Task code

--- edu/ucalgary/oop/TaskType.java
+++ edu/ucalgary/oop/TaskType.java
@@ -1,7 +1,17 @@
 package edu.ucalgary.oop;
 
+/**
+ * Represents the different types of tasks that can be assigned.
+ * This enumeration contains the following types: MEDICAL, CLEANING, and
+ * FEEDING.
+ * 
+ * @author Aarsh
+ */
 public enum TaskType {
+    // Represents a task related to medical care or procedures
     MEDICAL,
+    // Represents a task related to cleaning or maintenance
     CLEANING,
+    // Represents a task related to feeding or providing nutrition
     FEEDING;
 }
