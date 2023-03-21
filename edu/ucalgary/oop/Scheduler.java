5e3c9549d8d6adaba59ac3c31be06cc764a9b341
Will <williamgfraser2015@gmail.com>
2023-03-21 13:46:54 -0600

    Fixed spelling error in sql, emptied Scheduler and DailySchedule to fix

--- edu/ucalgary/oop/Scheduler.java
+++ edu/ucalgary/oop/Scheduler.java
@@ -4,43 +4,5 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Scheduler {
-    private LocalDate currDate;
-    private ArrayList<DailySchedule> schedules;
-
-    public Scheduler(String[] animals, String[] tasks) throws IllegalArgumentException {
-        // TODO implement constructor
-        // Change this up, it's just a placeholder to avoid final variable not
-        // initialized error
-        this.currDate = LocalDate.now();
-        this.schedules = new ArrayList<DailySchedule>();
-    }
-
-    public void printOutSchedule(LocalDate chosenDate) {
-        // TODO implement method
-    }
-
-    public void createFile() {
-        // TODO implement method
-    }
-
-    public ArrayList<DailySchedule> getSchedules() {
-        return schedules;
-    }
-
-    public void setSchedules(ArrayList<DailySchedule> newSchedules) {
-        this.schedules = newSchedules;
-    }
-
-    public void addSchedule(DailySchedule newSchedule) {
-        this.schedules.add(newSchedule);
-    }
-
-    public LocalDate getCurrDate() {
-        return currDate;
-    }
-
-    public void setCurrDate(LocalDate newCurrDate) {
-        this.currDate = newCurrDate;
-    }
 
 }
