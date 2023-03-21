299bc2b3953c9ae3461470c61fbb41492038fc84
NickSavino <nicksavino2@gmail.com>
2023-03-21 16:28:23 -0600

    Added testing for coyote fox and beaver, changed tests for SQLdatabase to match updated UML specifications

--- edu/ucalgary/oop/SQLDatabaseTest.java
+++ edu/ucalgary/oop/SQLDatabaseTest.java
@@ -7,50 +7,81 @@ import java.util.ArrayList;
 
 public class SQLDatabaseTest {
 
-
+    /**
+     * Test case for the SQLDatabase constructor with valid input.
+     * Creates an instance of the SQLDatabase class with a valid database name and asserts that the connection is not null.
+     */
     @Test
     public void testConstructorGoodInput() {
-        SQLDatabase db = new SQLDatabase("competition");
+        SQLDatabase db = new SQLDatabase("ewr");
         assertNotNull("Expected Database to exist", db.getConnection());
     }
 
-    @Test
+    /**
+     * Test case for the SQLDatabase constructor with invalid input.
+     * Creates an instance of the SQLDatabase class with an invalid database name and asserts that the connection is null.
+     */
+    @Test(expected = IllegalArgumentException.class)
     public void testConstructorBadInput() {
         SQLDatabase db = new SQLDatabase("badName");
         assertNull("Database should be null", db.getConnection());
+
     }
 
+    /**
+     * Test case for the getAnimals() method of the SQLDatabase class.
+     * Creates an instance of the SQLDatabase class, sets the list of animals, and asserts that the list of animals contains the correct values.
+     */
     @Test
     public void testGetAnimals() {
-        SQLDatabase db = new SQLDatabase("competition");
-        ArrayList<String> animals = new ArrayList<String>();
-        animals.add("dog");
-        animals.add("cat");
+        SQLDatabase db = new SQLDatabase("ewr");
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        Animal beaver = new Beaver(1, "John", AnimalType.COYOTE.toString(), FeedingType.CREPUSCULAR.toString(), false);
+        Animal coyote = new Coyote(2, "Tim", AnimalType.COYOTE.toString(), FeedingType.DIURNAL.toString(), false);
+
+        animals.add(beaver);
+        animals.add(coyote);
         db.setAnimals(animals);
+        assertNotNull(animals);
         assertEquals(2, db.getAnimals().size());
-        assertTrue(db.getAnimals().contains("dog"));
-        assertTrue(db.getAnimals().contains("cat"));
+        assertTrue(db.getAnimals().contains(beaver));
+        assertTrue(db.getAnimals().contains(coyote));
     }
 
+    /**
+     * Test case for the setAnimals() method of the SQLDatabase class.
+     * This test case is currently empty, and needs to be implemented.
+     */
     @Test
     public void testSetAnimals() {}
 
+    /**
+     * Test case for the getTasks() method of the SQLDatabase class.
+     * Creates an instance of the SQLDatabase class and asserts that the list of tasks is not null and has a size of 0.
+     */
     @Test
     public void testGetTasks() {
-        SQLDatabase database = new SQLDatabase("competition");
+        SQLDatabase database = new SQLDatabase("ewr");
         assertNotNull(database.getTasks());
         assertEquals(0, database.getTasks().size());
     }
 
+    /**
+     * Test case for the setTasks() method of the SQLDatabase class.
+     * Creates an instance of the SQLDatabase class, sets the list of tasks, and asserts that the list of tasks contains the correct values.
+     */
     @Test
     public void testSetTasks() {
-        SQLDatabase db = new SQLDatabase("competition");
-        ArrayList<String> tasks = new ArrayList<String>();
-        tasks.add("feed the animals");
-        tasks.add("clean the cages");
+        SQLDatabase db = new SQLDatabase("ewr");
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        Task task1 = new Task(1, "Feed Beaver", false, 30, 45, TaskType.FEEDING.toString());
+        Task task2 = new Task(2, "Treat Coyote", true, 60, 30, TaskType.MEDICAL.toString());
+        tasks.add(task1);
+        tasks.add(task2);
         db.setTasks(tasks);
+        assertNotNull(tasks);
         assertEquals(2, db.getTasks().size());
-        assertTrue(db.getTasks().contains("feed the animals"));
-        assertTrue(db.getTasks().contains("clean the cages"));
+        assertTrue(db.getTasks().contains(task1));
+        assertTrue(db.getTasks().contains(task2));
     }
 }
