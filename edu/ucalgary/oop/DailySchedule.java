b9001175990a846920a5262f5a3e8c665b60f489
Will <williamgfraser2015@gmail.com>
2023-03-22 15:16:15 -0600

    Completed the DailyScheduleTest class

--- edu/ucalgary/oop/DailySchedule.java
+++ edu/ucalgary/oop/DailySchedule.java
@@ -4,5 +4,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class DailySchedule {
+    private final LocalDate CURR_DATE;
+    private final ArrayList<String> TASK_AND_TIME;
+
+    public DailySchedule(ArrayList<Animal> animals, ArrayList<Task> tasks, LocalDate date) {
+        this.CURR_DATE = date;
+        // TODO implement this constructor
+    }
 
 }
--- edu/ucalgary/oop/DailyScheduleTest.java
+++ edu/ucalgary/oop/DailyScheduleTest.java
@@ -1,5 +1,86 @@
 package edu.ucalgary.oop;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
+
 public class DailyScheduleTest {
-    // TODO implement tests
+    /**
+     * @version 1.0
+     * @author Will
+     * @date 2023-03-21
+     */
+
+    // Tests the constructor with good input
+    // Uses example data from sql and expected output from the txt file
+    @Test
+    public void testConstructorGoodInput() {
+        LocalDate date = LocalDate.now();
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Fox(7, "Slinky", "fox", "NOCTURNAL", false));
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        tasks.add(new Task(2, "Rebandage leg wound", 20, 1, "MEDICAL"));
+        tasks.add(new Task(11, "feed fox", 10, 3, "FEEDING"));
+        DailySchedule schedule = new DailySchedule(animals, tasks, date);
+        assertEquals(2, schedule.getTaskAndTime().size());
+        assertEquals(date, schedule.getCurrDate());
+        assertEquals("00:00\n* Feeding - fox (Slinky)\n", schedule.getTaskAndTime().get(0));
+        assertEquals("19:00\n* Rebandage leg wound (Slinky)\n", schedule.getTaskAndTime().get(1));
+    }
+
+    // Tests that the constructor will throw an exception if the schedule is not
+    // possilbe
+    // Tests based on having 6 25 min tasks that must be done in one hour
+    // Even with an extra volunteer, this is not possible
+    @Test(expected = ImpossibleScheduleException.class)
+    public void testConstructorBadInput() {
+        LocalDate date = LocalDate.now();
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Coyote(5, "Eraser1", "coyote", "CREPUSCULAR", false));
+        animals.add(new Coyote(5, "Eraser2", "coyote", "CREPUSCULAR", false));
+        animals.add(new Coyote(5, "Eraser3", "coyote", "CREPUSCULAR", false));
+        animals.add(new Coyote(5, "Eraser4", "coyote", "CREPUSCULAR", false));
+        animals.add(new Coyote(5, "Eraser5", "coyote", "CREPUSCULAR", false));
+        animals.add(new Coyote(5, "Eraser6", "coyote", "CREPUSCULAR", false));
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        tasks.add(new Task(5, "Flush neck wound", 25, 1, "MEDICAL"));
+        tasks.add(new Task(5, "Flush neck wound", 25, 1, "MEDICAL"));
+        tasks.add(new Task(5, "Flush neck wound", 25, 1, "MEDICAL"));
+        tasks.add(new Task(5, "Flush neck wound", 25, 1, "MEDICAL"));
+        tasks.add(new Task(5, "Flush neck wound", 25, 1, "MEDICAL"));
+        tasks.add(new Task(5, "Flush neck wound", 25, 1, "MEDICAL"));
+        DailySchedule schedule = new DailySchedule(animals, tasks, LocalDate.now());
+    }
+
+    // Tests the getter for the current date
+    @Test
+    public void testGetCurrDate() {
+        LocalDate date = LocalDate.now();
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Fox(7, "Slinky", "fox", "NOCTURNAL", false));
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        tasks.add(new Task(2, "Rebandage leg wound", 20, 1, "MEDICAL"));
+        tasks.add(new Task(11, "feed fox", 10, 3, "FEEDING"));
+        DailySchedule schedule = new DailySchedule(animals, tasks, date);
+        assertEquals(date, schedule.getCurrDate());
+    }
+
+    // Tests the getter for the task and time ArrayList
+    @Test
+    public void testGetTaskAndTime() {
+        LocalDate date = LocalDate.now();
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("00:00\n* Feeding - fox (Slinky)\n");
+        expected.add("19:00\n* Rebandage leg wound (Slinky)\n");
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Fox(7, "Slinky", "fox", "NOCTURNAL", false));
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        tasks.add(new Task(2, "Rebandage leg wound", 20, 1, "MEDICAL"));
+        tasks.add(new Task(11, "feed fox", 10, 3, "FEEDING"));
+        DailySchedule schedule = new DailySchedule(animals, tasks, date);
+        assertEquals(expected, schedule.getTaskAndTime());
+    }
+
 }
