4a47fc8dd5b2621438996b23435e5342ef735a38
NickSavino <nicksavino2@gmail.com>
2023-04-01 15:11:34 -0600

    Added Popup for event where volunteer is needed and not checked

--- edu/ucalgary/oop/GUI.java
+++ edu/ucalgary/oop/GUI.java
@@ -2,6 +2,8 @@ package edu.ucalgary.oop;
 
 import javax.swing.*;
 import javax.swing.border.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,13 +18,18 @@ public class GUI extends JFrame {
 
     private JLabel topHeader;
 
-    private JScrollPane schedulePanel;
+    private JPanel topPanel;
+    private JPanel schedulePanel;
+    private JPanel buttonsPanel;
 
-    private JMenuBar menu;
+    private JMenuBar menuBar;
+
+    JCheckBoxMenuItem volunteerCheck;
 
     public GUI() {
         super("EWR schedule manager");
         setupGUI();
+        
         setSize(WIDTH,HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -32,61 +39,58 @@ public class GUI extends JFrame {
         buildMenuBar();
 
         topHeader = new JLabel("EWR schedule manager");
+        topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+        
+        topPanel.add(menuBar, BorderLayout.NORTH);
+        topPanel.add(topHeader, BorderLayout.SOUTH);
+        topPanel.setPreferredSize(new Dimension(WIDTH, (int) (HEIGHT * ((double) 0.2))));
 
-        JPanel topPanel = new JPanel();
-        topPanel.setLayout(new FlowLayout());
-        topPanel.add(topHeader);
+        
 
-        schedulePanel = new JScrollPane();
-        schedulePanel.setLayout(new ScrollPaneLayout());
+        buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+        buttonsPanel.setBorder(BorderFactory.createTitledBorder("Options"));
+
+        JButton createButton = new JButton();
+        createButton.setText("Create Schedule");
+        JButton manageButton = new JButton();
+        manageButton.setText("Manage Schedule");
+        JButton printButton = new JButton();
+        printButton.setText("Print Schedule");
+        printButton.addActionListener(printButtonEvent -> printSchedule());
+        volunteerCheck = new JCheckBoxMenuItem();
+
+        buttonsPanel.add(createButton);
+        buttonsPanel.add(manageButton);
+        buttonsPanel.add(printButton);
+        buttonsPanel.add(volunteerCheck);
 
 
-        Border border = BorderFactory.createTitledBorder("Schedule");
-        schedulePanel.setBounds(0, 0, 200, 200);
-        schedulePanel.setBorder(border);
-        schedulePanel.setPreferredSize(new Dimension(200, 200));
-        schedulePanel.setAlignmentY(TOP_ALIGNMENT);
+        schedulePanel = new JPanel();
+        schedulePanel.setLayout(new BorderLayout());
+        schedulePanel.setBorder(BorderFactory.createTitledBorder("Schedule"));
+        schedulePanel.setPreferredSize(new Dimension(200, (int) (HEIGHT * ((double) 0.6))));
 
         this.add(topPanel, BorderLayout.NORTH);
         this.add(schedulePanel, BorderLayout.WEST);
+        this.add(buttonsPanel, BorderLayout.EAST);
 
-        try {
-            getSchedule();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    
+        getSchedule();
+       
     }
 
-    public void getSchedule() throws FileNotFoundException {
-
-        BufferedReader reader;
-
-        File schedule = new File("schedule.txt");
-        if (!schedule.exists()) {
-            throw new FileNotFoundException("File does not exist");
-            
-        }
-        System.out.println("File exists");
-        
-        JLabel items = new JLabel();
-        items.setVerticalAlignment(JLabel.TOP);
+    public void getSchedule() {
 
         try {
-            reader = new BufferedReader(new FileReader(schedule));
-            String line = "empty";
-            while (line != null) {
-                line = reader.readLine();
-                if (line != null) {
-                    System.out.println(line);
-                    items.add(new JLabel(line));
-                }
-            }
-            schedulePanel.add(items);
-
+            BufferedReader reader = new BufferedReader(new FileReader("schedule.txt"));
+            String line = reader.readLine();
+            String date = "";
+            JLabel lastTimeLabel = null;
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public void buildMenuBar() {
@@ -105,11 +109,72 @@ public class GUI extends JFrame {
         helpMenu.add (aboutItem);
 
         //construct menubar
-        menu = new JMenuBar();
-        menu.add (fileMenu);
-        menu.add (helpMenu);
-        this.setJMenuBar(menu);
+        menuBar = new JMenuBar();
+        menuBar.add (fileMenu);
+        menuBar.add (helpMenu);
+        this.setJMenuBar(menuBar);
     }
+
+
+    
+    /**
+     * @version 1.0.0
+     * @author Nicola Savino
+     * @since 2020-11-20
+     * 
+     * This method is called when the print schedule button is pressed. 
+     * It checks if the volunteer checkbox is checked, and if it is not, 
+     * it will display a popup window asking the user to confirm that a volunteer is present.
+     */
+    public void printSchedule() {
+
+        if (!volunteerCheck.isSelected()) {
+
+            JFrame volunteerConfirmation = new JFrame("Volunteer Confirmation");
+            volunteerConfirmation.setSize(300, 100);
+            volunteerConfirmation.setLocationRelativeTo(this);
+            volunteerConfirmation.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            volunteerConfirmation.setVisible(true);
+            volunteerConfirmation.setResizable(false);
+
+            
+            JLabel volunteerLabel = new JLabel("Please confirm that a volunteer is present");
+            
+            volunteerLabel.setPreferredSize(new Dimension(300, 50));
+            //volunteerLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+            volunteerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            volunteerConfirmation.add(volunteerLabel, BorderLayout.NORTH);
+
+            JButton closePopup = new JButton("Ok");
+            closePopup.setMaximumSize(new Dimension(100, 50));
+            closePopup.setMargin(new Insets(0, 10, 0, 10));;
+            closePopup.addActionListener(volunteerConfirmEvent -> volunteerConfirmation.dispose());
+            volunteerConfirmation.add(closePopup, BorderLayout.SOUTH);
+
+            // Disable the main frame
+            setEnabled(false);
+
+            // Add a window listener to the popup frame
+            volunteerConfirmation.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosed(java.awt.event.WindowEvent windowEvent) {
+                    // Re-enable the main frame when the popup frame is closed
+                    setEnabled(true);
+                    setVisible(true);
+                }
+            });
+        
+        }
+    }
+
+    public void createSchedule() {
+        //TODO
+    }
+
+    public void exit() {
+        //TODO
+    }
+
 
     public static void main(String[] args) {
 
@@ -117,5 +182,6 @@ public class GUI extends JFrame {
             new GUI().setVisible(true);
         });
     }
+
 }
 
