a79d19136a5213af19f30bb79b0b89d10c948ec9
Will <williamgfraser2015@gmail.com>
2023-04-01 13:32:39 -0600

    added constructor for dailySchedule

--- edu/ucalgary/oop/DailySchedule.java
+++ edu/ucalgary/oop/DailySchedule.java
@@ -2,14 +2,36 @@ package edu.ucalgary.oop;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.time.LocalTime;
+import java.util.Iterator;
 
 public class DailySchedule {
     private final LocalDate CURR_DATE;
-    private final ArrayList<String> TASK_AND_TIME;
+    private final HashMap<LocalTime, ArrayList<String>> SCHEDULED_TASKS;
+    private ArrayList<Animal> animals;
+    private ArrayList<Task> tasks;
+    private ArrayList<Treatment> treatments;
 
-    public DailySchedule(ArrayList<Animal> animals, ArrayList<Task> tasks, LocalDate date) {
+    public DailySchedule(ArrayList<Animal> animals, ArrayList<Task> tasks, ArrayList<Treatment> treatments,
+            LocalDate date) {
         this.CURR_DATE = date;
-        // TODO implement this constructor
+        this.animals = animals;
+        this.treatments = treatments;
+        this.tasks = tasks;
+
+        // Add inferred tasks
+        tasks.add(new Task(tasks.size() + 1, "Feeding - coyote", 5, 3));
+        tasks.add(new Task(tasks.size() + 1, "Feeding - fox", 5, 3));
+        tasks.add(new Task(tasks.size() + 1, "Feeding - raccoon", 5, 3));
+        tasks.add(new Task(tasks.size() + 1, "Feeding - beaver", 5, 3));
+        tasks.add(new Task(tasks.size() + 1, "Feeding - porcupine", 5, 3));
+
+        this.SCHEDULED_TASKS = scheduleTasks();
+    }
+
+    private ArrayList<String> scheduleTasks() {
+
     }
 
 }
