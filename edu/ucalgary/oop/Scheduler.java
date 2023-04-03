77a6f40822055d0602954238e47090db66a01641
AarshShah9 <aarshshah99@gmail.com>
2023-04-02 21:56:40 -0600

    added call to SQLDatabase in sceduler

--- edu/ucalgary/oop/Scheduler.java
+++ edu/ucalgary/oop/Scheduler.java
@@ -4,29 +4,32 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Scheduler {
-    private ArrayList<Task> Tasks;
-    private ArrayList<Animal> Animals;
-    private ArrayList<Treatment> Treatments;
+    private ArrayList<Task> tasks;
+    private ArrayList<Animal> animals;
+    private ArrayList<Treatment> treatments;
     private LocalDate DATE;
     private DailySchedule dailySchedule;
 
-    public Scheduler(LocalDate day, ArrayList<Animal> animals, ArrayList<Task> tasks, ArrayList<Treatment> treatments) {
+    public Scheduler(LocalDate day) {
         this.DATE = day;
-        this.Animals = animals;
-        this.Tasks = tasks;
-        this.Treatments = treatments;
+        this.animals = new ArrayList<Animal>();
+        this.tasks = new ArrayList<Task>();
+        this.treatments = new ArrayList<Treatment>();
+        try {
+            SQLDatabase db = new SQLDatabase("EWR", animals, tasks, treatments);
+        } catch (Exception e) {
+            System.out.println("SQLDatabaseException caught: " + e.getMessage());
+            throw new IllegalArgumentException(e);
+        }
     }
 
     public DailySchedule calculateSchedule() {
         try {
-            this.dailySchedule = new DailySchedule(Animals, Tasks, Treatments, DATE);
-        }
-        catch(Exception e) {
+            this.dailySchedule = new DailySchedule(animals, tasks, treatments, DATE);
+        } catch (Exception e) {
             System.out.println("ImpossibleScheduleException caught: " + e.getMessage());
         }
-        finally {
-            return dailySchedule;
-        }
+        return dailySchedule;
     }
 
     public LocalDate getDate() {
@@ -34,26 +37,26 @@ public class Scheduler {
     }
 
     public ArrayList<Animal> getAnimals() {
-        return Animals;
+        return animals;
     }
 
     public void setAnimals(ArrayList<Animal> newAnimals) {
-        this.Animals = newAnimals;
+        this.animals = newAnimals;
     }
 
     public ArrayList<Task> getTasks() {
-        return Tasks;
+        return tasks;
     }
 
     public void setTasks(ArrayList<Task> newTasks) {
-        this.Tasks = newTasks;
+        this.tasks = newTasks;
     }
 
     public ArrayList<Treatment> getTreatments() {
-        return Treatments;
+        return treatments;
     }
 
     public void setTreatments(ArrayList<Treatment> newTreatments) {
-        this.Treatments = newTreatments;
+        this.treatments = newTreatments;
     }
 }
