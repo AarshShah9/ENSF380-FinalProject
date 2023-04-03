ed11954e2b7add80fcb3dead375fbef09bd986d1
AarshShah9 <aarshshah99@gmail.com>
2023-04-03 15:07:30 -0600

    fixed animals classes

--- edu/ucalgary/oop/Beaver.java
+++ edu/ucalgary/oop/Beaver.java
@@ -5,9 +5,9 @@ public class Beaver extends Animal {
     // public static final LocalTime FEED_PREP_TIME = LocalTime.of(0, 0, 0, 0);
     // public static final LocalTime CLEAN_TIME = LocalTime.of(0, 5, 0, 0);
 
-    public Beaver(int id, String animalName, String animalType)
+    public Beaver(int id, String animalName)
             throws IllegalArgumentException {
-        super(id, animalName, animalType, 5, 0, 5, FeedingType.DIURNAL);
+        super(id, animalName, AnimalType.BEAVER, 5, 0, 5, FeedingType.DIURNAL);
     }
 
 }
--- edu/ucalgary/oop/Fox.java
+++ edu/ucalgary/oop/Fox.java
@@ -1,15 +1,10 @@
 package edu.ucalgary.oop;
 
-import java.time.LocalTime;
-
 public class Fox extends Animal {
-    // public static final LocalTime FEED_TIME = LocalTime.of(0, 5, 0, 0);
-    // public static final LocalTime FEED_PREP_TIME = LocalTime.of(0, 5, 0, 0);
-    // public static final LocalTime CLEAN_TIME = LocalTime.of(0, 5, 0, 0);
 
-    public Fox(int id, String animalName, String animalType)
+    public Fox(int id, String animalName)
             throws IllegalArgumentException {
-        super(id, animalName, animalType, 5, 5, 5, FeedingType.NOCTURNAL);
+        super(id, animalName, AnimalType.FOX, 5, 5, 5, FeedingType.NOCTURNAL);
         // porpcupines are crepuscular
         // foxes & racoons are noctural
         // beavers are diurnal
