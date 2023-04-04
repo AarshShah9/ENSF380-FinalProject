140d5e23cb2f54368c099fb0bcfe7d52a6634d30
AarshShah9 <aarshshah99@gmail.com>
2023-04-03 19:05:31 -0600

    cleaned repo

--- edu/ucalgary/oop/Beaver.java
+++ edu/ucalgary/oop/Beaver.java
@@ -1,9 +1,6 @@
 package edu.ucalgary.oop;
 
 public class Beaver extends Animal {
-    // public static final LocalTime FEED_TIME = LocalTime.of(0, 5, 0, 0);
-    // public static final LocalTime FEED_PREP_TIME = LocalTime.of(0, 0, 0, 0);
-    // public static final LocalTime CLEAN_TIME = LocalTime.of(0, 5, 0, 0);
 
     public Beaver(int id, String animalName)
             throws IllegalArgumentException {
